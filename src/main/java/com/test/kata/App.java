2c0521d78314c9aeefd04aca855001b9a0bab41e
musangbeku <atheros.vajra@gmail.com>
2019-06-22 01:19:10 +0700

    update

--- src/main/java/com/test/kata/App.java
+++ src/main/java/com/test/kata/App.java
@@ -5,6 +5,6 @@ import com.test.kata.hackerrank.HackerrankJune;
 public class App {
 
     public static void main(String[] args) {
-        HackerrankJune.acmTeam(new String[] {"10101", "11110", "00010"});
+        System.out.println(HackerrankJune.weightedUniformStrings("aaabbbbcccddd", new int[] {9, 7, 8, 12, 5}));
     }
 }
--- src/main/java/com/test/kata/hackerrank/HackerrankJune.java
+++ src/main/java/com/test/kata/hackerrank/HackerrankJune.java
@@ -1,8 +1,8 @@
 package com.test.kata.hackerrank;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public final class HackerrankJune {
 
@@ -36,20 +36,18 @@ public final class HackerrankJune {
     }
 
     public static String[] cavityMap(String[] grid) {
-        for (int i = 0; i < grid.length; i++) {
-            if (i != grid.length - 1 && i != 0) {
-                char[] a = grid[i - 1].toCharArray();
-                char[] b = grid[i].toCharArray();
-                char[] c = grid[i + 1].toCharArray();
-                for (int j = 0; j < a.length; j++) {
-                    if (j != a.length - 1 && j != 0
-                            && b[j] > b[j - 1] && b[j] > b[j + 1]
-                            && b[j] > a[j] && b[j] > c[j]) {
-                        b[j] = 'X';
-                    }
+        for (int i = 1; i < grid.length - 1; i++) {
+            char[] a = grid[i - 1].toCharArray();
+            char[] b = grid[i].toCharArray();
+            char[] c = grid[i + 1].toCharArray();
+            for (int j = 0; j < a.length; j++) {
+                if (j != a.length - 1 && j != 0
+                        && b[j] > b[j - 1] && b[j] > b[j + 1]
+                        && b[j] > a[j] && b[j] > c[j]) {
+                    b[j] = 'X';
                 }
-                grid[i] = String.valueOf(b);
             }
+            grid[i] = String.valueOf(b);
         }
         return grid;
     }
@@ -77,5 +75,133 @@ public final class HackerrankJune {
             }
         }
         return new int[] {max, Collections.frequency(integers, max)};
+    }
+
+    public static int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2) {
+        if (y1 > y2) {
+            return 10000;
+        } else if (m1 > m2 && y1 >= y2) {
+            return (m1 - m2) * 500;
+        } else if (d1 > d2 && m1 >= m2 && y1 >= y2) {
+            return (d1 - d2) * 15;
+        } else {
+            return 0;
+        }
+    }
+
+    public static String superReducedString(String s) {
+        List<Character> characters = new ArrayList<>();
+        for (char c : s.toCharArray()) {
+            characters.add(c);
+        }
+        int i = 0;
+        while (i < characters.size() - 1) {
+            if (characters.get(i) == characters.get(i + 1)) {
+                characters.remove(i + 1);
+                characters.remove(i);
+                i = 0;
+            } else {
+                i++;
+            }
+        }
+        if (characters.isEmpty()) {
+            return "Empty String";
+        } else {
+            StringBuilder sb = new StringBuilder();
+            for (char c : characters) {
+                sb.append(c);
+            }
+            return sb.toString();
+        }
+    }
+
+    public static int camelcase(String s) {
+        String[] arr = s.split("[A-Z]");
+        return arr.length;
+    }
+
+    public static int minimumNumber(int n, String password) {
+        String[] regex = {"[0-9]", "[a-z]", "[A-Z]", "[!@#$%^&*()\\-\\+]"};
+        Pattern p;
+        Matcher m;
+        int regexCount = 0;
+        for (String s : regex) {
+            p = Pattern.compile(s);
+            m = p.matcher(password);
+            if (m.find()) {
+                regexCount++;
+            }
+        }
+        if (n < 6) {
+            if (4 - regexCount + n >= 6) {
+                return 4 - regexCount;
+            } else {
+                return 6 - n;
+            }
+        } else {
+            return 4 - regexCount;
+        }
+    }
+
+    public static int marsExploration(String s) {
+        int result = 0;
+        char[] sos = {'S', 'O' ,'S'};
+        int i = 0;
+        int j = 0;
+        while (i < s.length()) {
+            if (s.charAt(i) != sos[j]) {
+                result++;
+            }
+            if (j == sos.length - 1) {
+                j = 0;
+            } else {
+                j++;
+            }
+            i++;
+        }
+        return result;
+    }
+
+    public static String hackerrankInString(String s) {
+        String template = "hackerrank";
+        int count = 0;
+        int j = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == template.charAt(j)) {
+                count++;
+                j++;
+            }
+            if (count == template.length()) {
+                break;
+            }
+        }
+        return count == template.length() ? "YES" : "NO";
+    }
+
+    public static String pangrams(String s) {
+        s = s.toLowerCase().replaceAll(" ", "");
+        Set<Character> alphabets = new HashSet<>();
+        for (char c : s.toCharArray()) {
+            alphabets.add(c);
+        }
+        return alphabets.size() < 26 ? "not pangram" : "pangram";
+    }
+
+    public static String[] weightedUniformStrings(String s, int[] queries) {
+        String alphabets = "abcdefghijklmnopqrstuvwxyz";
+        int[] values = {1, 2, 3 , 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 18, 19, 21, 22, 23, 24, 25, 26};
+        List<Integer> sums = new ArrayList<>();
+        String[] answers = new String[queries.length];
+        for (int i = 0; i < s.length(); i++) {
+            if (i != 0 && s.charAt(i) == s.charAt(i - 1)) {
+                sums.add(values[alphabets.indexOf(String.valueOf(s.charAt(i)))] + sums.get(i - 1));
+            } else {
+                sums.add(values[alphabets.indexOf(String.valueOf(s.charAt(i)))]);
+            }
+        }
+        for (int i = 0; i < queries.length; i++) {
+            answers[i] = sums.contains(queries[i]) ? "Yes" : "No";
+        }
+        return answers;
     }
 }
